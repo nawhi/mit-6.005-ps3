c1179c9bc90bf2053649b062616327681b4baa70
Nick White <nawhi3@outlook.com>
2018-04-18 08:50:40 +0100

    Implement Variable

--- src/expressivo/Variable.java
+++ src/expressivo/Variable.java
@@ -2,8 +2,20 @@ package expressivo;
 
 public class Variable extends Primitive {
 
+	// Representation invariant:
+	//   true
+	// Safety from rep exposure:
+	// immutable type and use of immutable String 
+	// internally (means copies are always returned)
+	
+	private final String ident;
+	
 	public Variable(String ident) {
-		throw new RuntimeException("not implemented");
+		this.ident = ident;
+	}
+	
+	public String toString() {
+		return ident;
 	}
 
 }
