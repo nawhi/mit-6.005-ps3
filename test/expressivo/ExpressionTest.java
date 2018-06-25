575578007fae83e3802696b697acb041462a062f
Nick White <nawhi3@outlook.com>
2018-06-25 15:04:59 +0100

    Use whitespace & hash code flags in test methods

--- test/expressivo/ExpressionTest.java
+++ test/expressivo/ExpressionTest.java
@@ -13,15 +13,20 @@ import org.junit.Test;
  */
 public class ExpressionTest {
 	
-    @Test(expected=AssertionError.class)
+    // Flags for additional equality tests
+	private static final int IGNORE_WS = 0x1;
+    private static final int TEST_HC   = 0x2;
+    private static final int ALL_FLAGS = IGNORE_WS & TEST_HC;
+	
+	@Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
     @Test
     public void testWhitespaceDoesntMatter() {
-    	assertEquals("Whitespace shouldn't matter", "     hello      ", "hello");
-    	assertEquals("Whitespace shouldn't matter", "  H  e ll o \t", "\t\t\tHello");
+    	assertEquals("Whitespace shouldn't matter", "     hello      ", "hello", IGNORE_WS);
+    	assertEquals("Whitespace shouldn't matter", "  H  e ll o \t", "\t\t\tHello", IGNORE_WS);
     }
 
 	/*
@@ -47,9 +52,9 @@ public class ExpressionTest {
 	@Test public void testVariablesAlone() {
 		Variable v = new Variable("foo");
 		assertEquals("Variable's string representation should be correct", 
-				"foo", v.toString());
-		assertTrue("Variables with the same ident should equals() each other", 
-				new Variable("foo").equals(new Variable("foo")));
+				"foo", v.toString(), IGNORE_WS);
+		assertEquals("Variables with the same ident should equal each other", 
+				new Variable("foo"), new Variable("foo"));
 	}
 	
 	@Test public void testNumbersAlone() {
@@ -90,27 +95,27 @@ public class ExpressionTest {
 	@Test public void testSum() {
 		assertEquals("Identical sums should equal each other",
 				new Sum(new Numeric("1"), new Variable("foo")),
-				new Sum(new Numeric("1"), new Variable("foo")));
+				new Sum(new Numeric("1"), new Variable("foo")), ALL_FLAGS);
 		assertEquals("Identical sums' string representations should equal each other",
 				new Sum(new Variable("foo"), new Variable("bar")).toString(),
-				new Sum(new Variable("foo"), new Variable("bar")).toString());
+				new Sum(new Variable("foo"), new Variable("bar")).toString(), ALL_FLAGS);
 		Sum s1 = new Sum(new Numeric("1"), new Numeric("2"));
 		Sum s2 = new Sum(new Numeric("3"), new Numeric("4"));
 		assertEquals("Sums should commute without arbitrary parentheses", 
-				"1+2+3+4", new Sum(s1, s2).toString());
+				"1+2+3+4", new Sum(s1, s2).toString(), IGNORE_WS);
 	}
 	
 	@Test public void testProduct() {
 		assertEquals("Identical products should equal each other",
 				new Product(new Numeric("1"), new Variable("foo")),
-				new Product(new Numeric("1"), new Variable("foo")));
+				new Product(new Numeric("1"), new Variable("foo")), ALL_FLAGS);
 		assertEquals("Identical products' string representations should equal each other",
 				new Product(new Variable("foo"), new Variable("bar")).toString(),
-				new Product(new Variable("foo"), new Variable("bar")).toString());
+				new Product(new Variable("foo"), new Variable("bar")).toString(), IGNORE_WS);
 		Product p1 = new Product(new Numeric("1"), new Numeric("2"));
 		Product p2 = new Product(new Numeric("3"), new Numeric("4"));
 		assertEquals("Products should commute without arbitrary parentheses",
-				"1*2*3*4", new Product(p1, p2).toString());
+				"1*2*3*4", new Product(p1, p2).toString(), IGNORE_WS);
 	}
 	
 	@Test public void testBinopStructuralEquality() {
@@ -118,8 +123,12 @@ public class ExpressionTest {
 		Variable b = new Variable("b");
 		assertNotEquals("Sums with swapped lvalue/rvalue should not compare equal",
 				new Sum(a, b), new Sum(b, a));
+		assertNotEquals("Sums with swapped lvalue/rvalue should not hash equal",
+				new Sum(a, b).hashCode(), new Sum(b, a).hashCode());
 		assertNotEquals("Products with swapped lvalue/rvalue should not compare equal",
 				new Product(a, b), new Product(b, a)); 
+		assertNotEquals("Products with swapped lvalue/rvalue should not hash equal",
+				new Product(a, b).hashCode(), new Product(b, a).hashCode()); 
 	}
 	
 	@Test public void testBinopObservationalEquality() {
@@ -132,16 +141,16 @@ public class ExpressionTest {
 		Expression multFirst = new Sum(new Product(a,a), new Numeric("1"));
 		Expression addFirst = new Product(new Sum(a, new Numeric("1")), a);
 				
-		assertEquals("Shouldn't add parens unless necessary", "a*a+1", multFirst.toString());
-		assertEquals("Should add parens to keep BIDMAS order", "(a+1)*a", addFirst.toString());
+		assertEquals("Shouldn't add parens unless necessary", "a*a+1", multFirst.toString(), IGNORE_WS);
+		assertEquals("Should add parens to keep BIDMAS order", "(a+1)*a", addFirst.toString(), IGNORE_WS);
 		assertFalse("Non-commutative expressions in different order shouldn't be equal",
 				multFirst.equals(addFirst));
 		
 		// These also check for any aliasing bugs by passing in the same object twice
 		assertEquals("Shouldn't add parens unless necessary in complex subexpressions",
-				"a*(a+1)*a*(a+1)", new Product(addFirst, addFirst).toString());
+				"a*(a+1)*a*(a+1)", new Product(addFirst, addFirst).toString(), IGNORE_WS);
 		assertEquals("Should add parens to keep BIDMAS in complex subexpressions", 
-				"(a*a+1)*(a*a+1)", new Product(multFirst, multFirst).toString());
+				"(a*a+1)*(a*a+1)", new Product(multFirst, multFirst).toString(), IGNORE_WS);
 
 	}
 	
@@ -149,14 +158,23 @@ public class ExpressionTest {
 	 * Whitespace does not matter in tests here, so ignore it in string
 	 * equality tests
 	 */
-	void assertEquals(String msg, Object expected, Object actual) {
-		if (expected instanceof String && actual instanceof String) {
+	void assertEquals(String msg, Object expected, Object actual, int flags) {
+		if (expected instanceof String 
+				&& actual instanceof String
+				&& (flags & IGNORE_WS) != 0) {
 			String o1 = ((String) expected).replaceAll("\\s", "");
 			String o2 = ((String) actual).replaceAll("\\s", "");
 			Assert.assertEquals(msg, o1, o2);
 		} else {
 			Assert.assertEquals(msg, expected, actual);
 		}
+		if ((flags & TEST_HC) != 0) {
+			Assert.assertEquals(msg, expected.hashCode(), actual.hashCode());
+		}
+	}
+	
+	void assertEquals(String msg, Object expected, Object actual) {
+		Assert.assertEquals(msg, expected, actual);
 	}
 	
 }
