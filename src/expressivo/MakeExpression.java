e4bff3c2142230df6669c3289842114801edafd4
Nick White <nawhi3@outlook.com>
2018-07-09 10:05:31 +0100

    Add stub MakeExpression generator

---
+++ src/expressivo/MakeExpression.java
@@ -0,0 +1,74 @@
+package expressivo;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import expressivo.parser.ExpressionListener;
+import expressivo.parser.ExpressionParser.PrimitiveContext;
+import expressivo.parser.ExpressionParser.RootContext;
+import expressivo.parser.ExpressionParser.SumContext;
+
+public class MakeExpression implements ExpressionListener {
+
+	@Override
+	public void enterEveryRule(ParserRuleContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exitEveryRule(ParserRuleContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitErrorNode(ErrorNode arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitTerminal(TerminalNode arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enterRoot(RootContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exitRoot(RootContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enterSum(SumContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exitSum(SumContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enterPrimitive(PrimitiveContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exitPrimitive(PrimitiveContext ctx) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
