fa2b82bcd50f788fd0da5d94721debdec79a45c3
Nick White <nawhi3@outlook.com>
2018-10-05 13:36:42 +0100

    Change Commands methods spec to accurately describe identifiers

--- src/expressivo/Commands.java
+++ src/expressivo/Commands.java
@@ -18,7 +18,7 @@ public class Commands {
     /**
      * Differentiate an expression with respect to a variable.
      * @param expression the expression to differentiate
-     * @param variable the variable to differentiate by, a case-sensitive nonempty string of letters.
+     * @param variable the variable to differentiate by, which much match the IDENT rule in the grammar
      * @return expression's derivative with respect to variable.  Must be a valid expression equal
      *         to the derivative, but doesn't need to be in simplest or canonical form.
      * @throws IllegalArgumentException if the expression or variable is invalid
@@ -30,9 +30,9 @@ public class Commands {
     /**
      * Simplify an expression.
      * @param expression the expression to simplify
-     * @param environment maps variables to values.  Variables are required to be case-sensitive nonempty 
-     *         strings of letters.  The set of variables in environment is allowed to be different than the 
-     *         set of variables actually found in expression.  Values must be nonnegative numbers.
+     * @param environment maps variables to values.  Variables are required to match the IDENT rule in the grammar.
+     *         The set of variables in environment is allowed to be different than the set of variables actually
+     *         found in expression.  Values must be nonnegative numbers.
      * @return an expression equal to the input, but after substituting every variable v that appears in both
      *         the expression and the environment with its value, environment.get(v).  If there are no
      *         variables left in this expression after substitution, it must be evaluated to a single number.
