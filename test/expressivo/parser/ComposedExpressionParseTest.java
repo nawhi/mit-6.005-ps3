731f73c9905f09d35cf8b3022a720111417c7e6d
nawhi <25394965+nawhi@users.noreply.github.com>
2019-04-13 16:18:50 +0100

    Keep cleaning up parse tests

---
+++ test/expressivo/parser/ComposedExpressionParseTest.java
@@ -0,0 +1,39 @@
+package expressivo.parser;
+
+import expressivo.expressions.Expression;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class ComposedExpressionParseTest {
+
+    @Test
+    @Parameters({
+            "a+b",
+            "x+3",
+            "3+x",
+            "0.9+3.356",
+            "x+y+z",
+            "1.2+3.5+f+98+2345+0.3"
+    })
+    public void sumsCanBeParsed(String input) {
+        assertThat(Expression.parse(input).toString()).isEqualTo(input);
+    }
+
+    @Test
+    @Parameters({
+            "a*b",
+            "x*3",
+            "3*x",
+            "0.9*3.356",
+            "x*y*z",
+            "1.2*3.5*f*98*2345*0.3"
+    })
+    public void productsCanBeParsed(String input) {
+        assertThat(Expression.parse(input).toString()).isEqualTo(input);
+    }
+}
--- test/expressivo/parser/SingleExpressionParseTest.java
+++ test/expressivo/parser/SingleExpressionParseTest.java
@@ -13,25 +13,27 @@ public class SingleExpressionParseTest {
 
 	@Test
 	@Parameters({
-			"0|0",
-			"1|1",
-			"01|1",
-			"10|10"
+			"0",
+			"1",
+			"10",
+
+			"1.5",
+			"3.1415926535",
 	})
-	public void integersCanBeParsed(String input, String expectedOutput) {
-		assertThat(Expression.parse(input).toString()).isEqualTo(expectedOutput);
+	public void numbersCanBeParsed(String input) {
+		assertThat(Expression.parse(input).toString()).isEqualTo(input);
 	}
 
 	@Test
 	@Parameters({
-			"1.5|1.5",
+			"01|1",
+			"0001|1",
 			".5|0.5",
-			"2.0|2", // OK for the time being
+			"2.0|2",
 			"2.300|2.3",
-			"3.1415926535|3.1415926535",
 			"0025.300|25.3"
 	})
-	public void floatsCanBeParsed(String input, String expectedOutput) {
+	public void zeroesAreStandardisedWhenParsingNumbers(String input, String expectedOutput) {
 		assertThat(Expression.parse(input).toString()).isEqualTo(expectedOutput);
 	}
 
@@ -44,32 +46,6 @@ public class SingleExpressionParseTest {
 			"_1f"
 	})
 	public void variablesCanBeParsed(String input) {
-		assertThat(Expression.parse(input).toString()).isEqualTo(input);
-	}
-
-	@Test
-	@Parameters({
-			"a+b",
-			"x+3",
-			"3+x",
-			"0.9+3.356",
-			"x+y+z",
-			"1.2+3.5+f+98+2345+0.3"
-	})
-	public void sumsCanBeParsed(String input) {
-		assertThat(Expression.parse(input).toString()).isEqualTo(input);
-	}
-
-	@Test
-	@Parameters({
-			"a*b",
-			"x*3",
-			"3*x",
-			"0.9*3.356",
-			"x*y*z",
-			"1.2*3.5*f*98*2345*0.3"
-	})
-	public void productsCanBeParsed(String input) {
 		assertThat(Expression.parse(input).toString()).isEqualTo(input);
 	}
 }
